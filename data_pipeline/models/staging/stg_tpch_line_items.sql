f70c1842f294615acea2d1c3fbd49a969ef869ba
Younes AMRANE <Younes.AMRANE@hardis-group.com>
2024-09-17 16:17:38 +0200

    create staging and source tables

---
+++ data_pipeline/models/staging/stg_tpch_line_items.sql
@@ -0,0 +1,19 @@
+select
+{{ 
+    dbt_utils.generate_surrogate_key
+    ([
+        'L_ORDERKEY',
+        'L_LINENUMBER'
+    ]) 
+}} as order_item_key,
+
+L_ORDERKEY as order_key,
+L_PARTKEY as part_key,
+L_LINENUMBER as line_number,
+L_QUANTITY as quantity,
+L_EXTENDEDPRICE as extended_price,
+L_DISCOUNT as discount,
+L_TAX as tax_rate
+
+
+from {{source('tpch','lineitem')}}
